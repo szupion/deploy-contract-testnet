b08b77c1b10c92e67860503bff0ec346abe57811
ispolin <ispolin3023@gmail.com>
2024-10-20 22:00:43 +0300

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,11 +1,1 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
-
-contract SimpleStorage {
-    string public name;
-
-    // Constructor that sets the contract name
-    constructor(string memory _name) {
-        name = _name;
-    }
-}
+код контракта 
